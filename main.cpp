4119fb5e79ffd5cd6f13fe8e1d9a3ce74295bcfb
youssef <youssefragab170@gmail.com>
2025-01-30 15:48:14 +0200

    filles added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,51 @@
+#include <algorithm>
 #include <iostream>
-using namespace std;
+#include <string>
+#include "bmplib.cpp"
+
+// SIZE = 256
+
+unsigned char image[SIZE][SIZE];
+
+// functions
+int loadImage () {
+    char imageFileName[100];
+
+    // Get gray scale image file name
+    cout << "Please enter file name of the image to process: ";
+    
+    cin >> imageFileName;
+    
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    if (readGSBMP(imageFileName, image)==1) {
+        return 0;
+    }
+    return 1;
+}
+
+void saveImage () {
+
+    char imageFileName[100];
+    // Get gray scale image target file name
+    cout << "\nEnter the target image file output name: ";
+
+    cin.ignore();
+    cin.getline(imageFileName,99);
+
+    // Add to it .bmp extension and load image
+    strcat (imageFileName, ".bmp");
+    writeGSBMP(imageFileName, image);
+}
+
+
 
 int main(){
-    int x ;
-    cin >> x ;
+    unsigned char image[SIZE][SIZE];
+    if(readGSBMP("photographer144.bmp", image) == 1){
+        cout << "error";
+    }
+    
+    writeGSBMP("photographer3.bmp", image);
 }
